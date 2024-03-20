bc3fd325d3963e8b06899e3ad36abf13a88200a8
Emily Ott <161410357+Em-Ott@users.noreply.github.com>
2024-03-19 20:21:49 -0700

    Add files via upload
    Added basic instructions, a way to tell black and white apart, and a way to tell if the game has started. I may change it into game ongoing, I am currently working through finding out how to make multiple files work with having one file with all of the header information.
    I seem to have connected all the files but am struggling on having using namespace std in all of the files.

--- chess.cpp
+++ chess.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <sstream> 
-
-using namespace std;
-
 /*
 Splitting up code: 
 chess.cpp, has main function which:
@@ -14,6 +9,7 @@ user input, determining if it's valid, and if it will kill a piece
 
 int main(){
     bool gameWon = false;
+    int gameStart = 0;
     char piece = '_';
     //the starting spots for all chess pieces are their initial value
     //pieces 0-7, 0-1, 0, lower number pieces are white
@@ -58,30 +54,71 @@ int main(){
     queen[1] = 3;
     king[1] = 4;
 
+    //explantion of rules
+    cout << "The pieces are abbreviated as the following: " << endl << "Pawn: p" << endl << 
+    "Rook: r" << endl << "Horse: h" << endl << "Bishop: b" << endl << "Queen: q" << endl <<
+    "King: k" << endl << "The white pieces are capital letters and the black pieces are lower case." << endl;
+    cout << "To begin type 1." << endl;
+    cin >> gameStart;
+
+    while (gameStart == 0 || cin.fail()){
+    cin.clear();
+    cin.ignore(256, '\n');
+    cin >> gameStart;
+    }
     /*
-    If statement to create the board with pieces in the accurate place.
+    If the game has started create the board with pieces in the accurate place.
     Using a count of 64, because 64 total spaces and it makes labeling and moving the pieces easier
     */
+   if (gameStart == 1){
    for (int i = 0; i < 64; ++i){
     //below statement will check if any piece takes up that spot on the boards. 16 because it is the max # of pieces
+    /*
+    Maybe capitals for white or black check number of j to see what it is
+    
+    */
     for (int j = 0; j < 16; ++j){
         if(pawn[j] == i){
-            piece = 'p';
+            if (j < 8){
+                piece = 'P';        
+            } else {
+                piece = 'p';
+            }
             break; //used so the loop doesn't continue once we know if there is a piece
         } else if (rook[j] == i){
-            piece = 'r';
+            if (j < 2){
+                piece = 'R';        
+            } else {
+                piece = 'r';
+            }
             break;
         } else if (horse[j] == i){
-            piece = 'h';
+            if (j < 2){
+                piece = 'H';
+            } else {
+                piece = 'h';
+            }
             break;
         } else if (bishop[j] == i){
-            piece = 'b';
+            if (j < 2){
+                piece = 'B';
+            } else {
+                piece = 'b';
+            }
             break;
         } else if (queen[j] == i){
-            piece = 'q';
+            if (j < 1){
+                piece = 'Q';
+            } else {
+                piece = 'q';
+            }
             break;
         } else if (king[j] == i){
-            piece = 'k';
+            if (j < 1){
+                piece = 'K';
+            } else {
+                piece = 'k';
+            }
             break;
         } else {
             piece = '_';
@@ -97,6 +134,7 @@ int main(){
             cout << endl;
         }
         cout << "|" << piece << "|";
+   }
    }
 
     return 0;
---
+++ chess.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "chessInput.cpp"
+#include "chess.cpp"
+
+using namespace std;
+
+int main();
